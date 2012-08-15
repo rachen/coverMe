26e6d43a1eb3142840fe45f5157e5e62f4904c2c
Raymond Chen <rchen009@gmail.com>
2012-08-15 01:40:57 -0700

    recommitting oweMe folder

---
+++ oweMe/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import patterns, include, url
+import core, users
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'oweMe.views.home', name='home'),
+    # url(r'^oweMe/', include('oweMe.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', "core.views.index"),
+    url(r'^login/$', 'users.views.login', name="login"),
+    url(r'^register/$', 'users.views.register', name="register"),
+    url(r'^logout/$', 'users.views.logout', name="logout"),
+
+)
